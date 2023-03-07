35931eae5ce14a52e03565b3fe123a2496efa908
Aleksey Bulatov <alekseybulatov.study@gmail.com>
2023-03-08 00:32:14 +0300

    Удобная для чтения

--- 02 - Красивая строка/beauty string.py
+++ 02 - Красивая строка/beauty string.py
@@ -5,6 +5,8 @@ n = len(s)
 cnt = 1
 max_cnt = 1
 prev = s[0]
+
+# макс одинаковых символов в тексе
 for i in range(1, n):
     if s[i] == prev:
         cnt += 1
@@ -12,8 +14,11 @@ for i in range(1, n):
         max_cnt = max(max_cnt, cnt)
         cnt = 1
         prev = s[i]
+
 max_cnt = max(max_cnt, cnt)
 
+# если разрешено производить достаточно замен, чтобы заменить все символы в строке,
+# выводим длину строки, иначе выполняем замены, чтобы увеличить количество одинаковых символов
 if k >= n:
     print(n)
 else:
@@ -21,15 +26,21 @@ else:
     freq = {s[l]: 1}
     max_freq = 1
     res = max_cnt
-    while r < n-1:
+
+    while r < n - 1:
         r += 1
+
         if s[r] not in freq:
             freq[s[r]] = 1
         else:
             freq[s[r]] += 1
         max_freq = max(max_freq, freq[s[r]])
-        if r - l + 1 - max_freq > k:
+
+        if r - l + 1 - max_freq > k:            # если количество замен, необходимых для получения подстроки с одинаковыми
+                                                # символами, больше, чем разрешено, сдвигаем левую границу подстроки
             freq[s[l]] -= 1
             l += 1
-        res = max(res, r-l+1)
+
+        res = max(res, r - l + 1)
+
     print(res)
